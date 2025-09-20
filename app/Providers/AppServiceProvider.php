8992acaffedf579479a1005bbd1b2c8fd1837772
IamRazer23 <66140727+IamRazer23@users.noreply.github.com>
2025-09-20 10:54:08 -0500

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // <-- importa esto
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Evita el error 1071 en índices únicos con utf8mb4
+        Schema::defaultStringLength(191);
     }
 }
